a842e97dabb5ad1679cff2b2d13fb32362aaf3dc
nvilisov13 <nvilisov13@gmail.com>
2024-04-04 19:53:58 +0300

    Working bot for testing employees. But without authentication employees.

--- handlers/users/on_start_testing.py
+++ handlers/users/on_start_testing.py
@@ -10,10 +10,12 @@ router = Router()
 
 @router.message(Command("onstart_test"))
 async def cmd_start(message: Message):
+    # print(message.model_dump_json(indent=4, exclude_none=True))
+    print(message.from_user.id)
     requests_test_info = requests.get("http://127.0.0.1:8000/drf_employees_test/")
     parsing_test_info = json.loads(requests_test_info.text)
     for key_test in parsing_test_info:
-        dictionary_tests = {f"✅ {key_test['id']}. {key_test['NameTest']}": key_test['id']}
+        dictionary_tests = {f"📝 {key_test['id']}. {key_test['NameTest']}": key_test['id']}
         await message.answer(
             f"<b><u>{key_test['DescriptionTest']}</u></b>",
             reply_markup=get_inline_kb(dictionary_tests, "showTests")
--- keyboards/for_questions.py
+++ keyboards/for_questions.py
@@ -3,14 +3,20 @@ from typing import Optional
 from aiogram.filters.callback_data import CallbackData
 
 
-class NumbersCallbackFactory(CallbackData, prefix='fabNum'):
+class NumbersCallbackFactory(CallbackData, prefix="fabNum"):
     action: str
-    value: Optional[int] = None
+    mark: Optional[int] = None
+    number: int
 
 
 def get_inline_kb(button_dict: dict, action_str: str):
     builder = InlineKeyboardBuilder()
+    number = 0
     for key, value in button_dict.items():
-        builder.button(text=key, callback_data=NumbersCallbackFactory(action=action_str, value=value))
+        builder.button(text=key, callback_data=NumbersCallbackFactory(action=action_str, mark=value, number=number))
+        number += 1
+    if action_str == "selectAnswer":
+        builder.button(text=f"✔ Подтвердить ответ", callback_data=NumbersCallbackFactory(action="confirmAnswers",
+                                                                                         number=number))
     builder.adjust(1)
     return builder.as_markup()
